6435096d71ad2b3255c3d70d820c10dd92fd3cba
lavochkin007 <45898178+lavochkin007@users.noreply.github.com>
2019-12-19 22:32:30 +0900

    修正

--- component---src-pages-about-jsx-6bd87258cbdf0041e68a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{bT9l:function(t,e,n){"use strict";n.r(e);n("q1tI");var a=n("TJpk"),c=n.n(a),b=n("17x9"),o=n.n(b),p=n("tCqT"),u=n("ehyU"),r=n("V7ww"),w=n("qKvR");e.default=function(t){return Object(w.d)(p.a,null,Object(w.d)(c.a,{title:"About Page"}),Object(w.d)(r.a,{title:"About Page"}),Object(w.d)(u.a,{center:t}))},u.a.propTypes={center:o.a.object}}}]);
-//# sourceMappingURL=component---src-pages-about-jsx-6bd87258cbdf0041e68a.js.map
